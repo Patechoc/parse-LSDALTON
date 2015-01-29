54c2416836ce57c7b5ae34c7f297cc1245e7dae7
Patrick Merlot <patrick.merlot@gmail.com>
2015-01-29 15:50:34 +0100

    not working, need to send proper csv data to plot.ly

--- plots/plotly_HistidineFerrocene.py
+++ plots/plotly_HistidineFerrocene.py
@@ -52,21 +52,21 @@ def get_data(mol_list, basis_list, dal_list, dal_ref, path_to_ref, path_to_dals)
         basis = basisVal.strip()
         results[basis] = {} 
         print basis
-        for molVal in mol_list:
-            mol  = molVal.strip()
-            results[basis][mol] = {} 
-            print "\t"+mol
-            file_ref = ""
-
-            cmd= "ls "+ path_to_ref+"/lsd*"+dal_ref[0]['LinK']+"*"+basis+"*"+mol+"*"
-            file_ref = run_command_or_exit(cmd).strip()
-            print "\t"+file_ref
-            dal_files = []
-            for dalObj in dal_list:
-                dft_func = dalObj['abrev'].strip()
-                dalPattern = dalObj['pattern'].strip()
-                print "\t\t"+dft_func
-                file_dal = ""
+        dal_files = []
+        for dalObj in dal_list:
+            dft_func = dalObj['abrev'].strip()
+            dalPattern = dalObj['pattern'].strip()
+            results[basis][dft_func] = {} 
+            print "\t\t"+dft_func
+            file_dal = ""
+            for molVal in mol_list:
+                mol  = molVal.strip()
+                print "\t"+mol
+                file_ref = ""
+                cmd= "ls "+ path_to_ref+"/lsd*"+dal_ref[0]['LinK']+"*"+basis+"*"+mol+"*"
+                file_ref = run_command_or_exit(cmd).strip()
+                print "\t"+file_ref
+                
                 cmd= "ls "+ path_to_dals+"/lsd*"+dalPattern+"*"+basis+"*"+mol+"*"
                 file_dal = run_command_or_exit(cmd).strip()
                 print "\t\t"+file_dal
@@ -75,81 +75,40 @@ def get_data(mol_list, basis_list, dal_list, dal_ref, path_to_ref, path_to_dals)
                 #print file_ref
                 diffGrad = compLS.get_compareInfoGradients(file_ref, file_dal)
                 #print diffGrad
-                results[basis][mol][dft_func] =  diffGrad['matDiffGrad'].flatten()
+                results[basis][dft_func][mol] =  diffGrad['matDiffGrad'].flatten()
     return results
 
 
 def generate_plot(results, today_str):
     ## DATA TO PLOT
-    y0 = np.random.randn(50)
-    y1 = np.random.randn(50)+1
-
-    trace1 = Box(
-        y=y0
-    )
-    trace4 = Box(
-        y=y1,
-        x='molecule 2'    
-    )
-
-    x = ['molecule 1', 'molecule 1', 'molecule 1', 'molecule 1', 'molecule 1', 
-         'molecule 2', 'molecule 2', 'molecule 2', 'molecule 2', 'molecule 2']
-
-
-    trace1 = Box(
-        y=[0.2, 0.2, 0.6, 1.0, 0.5, 0.4, 0.2, 0.7, 0.9, 0.1, 0.5, 0.3], ## diff. gradient elems 
-        x=x,## nb. atoms in molecule, ou molName
-        name='KT3X', # ADMM functional combined with basis set choice
-        marker=Marker(
-            color='#3D9970'),
-        boxpoints='all',
-        jitter=0.4,
-        pointpos=-0.0
-    )
-    trace2 = Box(
-        y=[0.6, 0.7, 0.3, 0.6, 0.0, 0.5, 0.7, 0.9, 0.5, 0.8, 0.7, 0.2],
-        x=x,
-        name='B88X',
-        marker=Marker(
-            color='#FF4136'
-        ),
-        boxpoints='all',
-        jitter=0.4,
-        pointpos=-0.0
-
-    )
-    trace3 = Box(
-        y=[0.1, 0.3, 0.1, 0.9, 0.6, 0.6, 0.9, 1.0, 0.3, 0.6, 0.8, 0.5],
-        x=x,
-        name='OPTX',
-        marker=Marker(
-            color='#FF851B'
-        ),
-        boxpoints='all',
-        jitter=0.4,
-        pointpos=-0.0
-    )
-
-
-
-    # trace2 = Scatter(
-    #     x=[0, 1, 2], # nb-atoms / name molecule
-    #     y=[3, 4, 6], # avg. difference over gradient difference
-    #     mode = 'markers',
-    #     name = 'monTrace2',
-    #     error_y=ErrorY(
-    #         type='data',
-    #         array=[1, 2, 3], # RMS norm
-    #         visible=True),
-    #     marker=Marker(
-    #         #color='#85144B',
-    #         size=8, )
-    # )
-
-    data = Data([trace1, trace2, trace3])
+    #y0 = np.random.randn(50)
+    #y1 = np.random.randn(50)+1
+    traces = []
+    myColors = get_colors()
+    
+    for regbase in results.keys():
+        numFunc = 0
+        for func in results[regbase].keys():
+            numFunc = numFunc+1
+            yX = []
+            xFunc = []
+            for mol in results[regbase][func].keys():
+                elemsPerMol = results[regbase][func][mol] ### elements of grad. diff
+                xFunc.append([mol for elem in elemsPerMol])
+                yX.append(elemsPerMol)
+            traceX = Box(y = yX,    ### diffGrad elements
+                         x = xFunc,   ### elements grouped on the same molecule
+                         name=func, ### ADMM functional combined for this basis set choice
+                         marker=Marker(color=myColors[numFunc]),
+                         boxpoints='all',
+                         jitter=0.4,
+                         pointpos=-0.0
+            )
+            traces.append(traceX)
+            data = Data(traces)
 
     layout = Layout(
-        title='Impact on molecular gradient'+today_str,
+        title='Impact on molecular gradient ('+today_str+')',
         xaxis=XAxis(
             title='set of molecules',
             # titlefont=Font(
@@ -169,8 +128,6 @@ def generate_plot(results, today_str):
         ),
         boxmode='group',
     )
-
-
     fig = Figure(data=data, layout=layout)
     plot_url = py.plot(fig, filename='Gradient differences')
 
