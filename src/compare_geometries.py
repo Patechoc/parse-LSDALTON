e63d7ae14f52db95ded2bb65f507ba6b5ad7222f
Patrick Merlot <patrick.merlot@gmail.com>
2015-03-24 13:46:17 +0100

    import RMSD successful

--- src/compare_geometries.py
+++ src/compare_geometries.py
@@ -3,15 +3,33 @@
 import sys, os, re, math
 import numpy as np
 import subprocess as subproc
+import imp
+#sys.path.append('../lib/RMSD/')
+#import calculate_rmsd as rmsd
+rmsd = imp.load_source("RMSD", "../lib/RMSD/calculate_rmsd")
 
-
-def get_RMS_deviation(path_to_file1, path_to_file2):
+def get_RMS_deviation(mol1, mol2):
     """
     Return the root-mean-square deviation between 2 geometries given in XYZ format.
     https://github.com/charnley/rmsd
     """
-    RMSdev = None
+    RMSdev = {}
+    atomsP, P = rmsd.get_coordinates(mol1)
+    atomsQ, Q = rmsd.get_coordinates(mol2)
 
+    # Calculate 'dumb' RMSD
+    normal_rmsd = rmsd.rmsd(P, Q)
+
+    # Create the centroid of P and Q which is the geometric center of a
+    # N-dimensional region and translate P and Q onto that center.
+    # http://en.wikipedia.org/wiki/Centroid
+    Pc = rmsd.centroid(P)
+    Qc = rmsd.centroid(Q)
+    P -= Pc
+    Q -= Qc
+    RMSdev["Normal RMSD"] = normal_rmsd
+    RMSdev["Kabsch RMSD"] = rmsd.kabsch_rmsd(P, Q)
+    RMSdev["Fitted RMSD"] = rmsd.fit(P, Q)
     return RMSdev
     
 
@@ -19,5 +37,7 @@ def get_RMS_deviation(path_to_file1, path_to_file2):
 # Testing
 # ============================================================================ #
 if __name__ == "__main__":
-    path_to_file1 = "./files/lsdalton_files/lsdalton20140924_geomOpt-b3lyp_Vanlenthe_6-31G_df-def2_Histidine_2CPU_16OMP_2014_10_28T1007.out"
+    path_to_file1 = "./files/Histidine_input.xyz"
+    path_to_file2 = "./files/Histidine_optimized.xyz"
     RMSdev = get_RMS_deviation(path_to_file1, path_to_file2)
+    print RMSdev
